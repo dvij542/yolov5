683d16718a8942811e00f38f3db5eb8c0831e4e0
dvij542 <dvij.kalaria@gmail.com>
2021-05-28 16:36:58 +0530

    changes

--- train.py
+++ train.py
@@ -88,6 +88,7 @@ def train(hyp, opt, device, tb_writer=None):
         model = Model(opt.cfg or ckpt['model'].yaml, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
         exclude = ['anchor'] if (opt.cfg or hyp.get('anchors')) and not opt.resume else []  # exclude keys
         state_dict = ckpt['model'].float().state_dict()  # to FP32
+        print(exclude)
         state_dict = intersect_dicts(state_dict, model.state_dict(), exclude=exclude)  # intersect
         model.load_state_dict(state_dict, strict=False)  # load
         print("State dict keys :", state_dict.keys())
