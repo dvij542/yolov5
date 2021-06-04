fe7b7cc5dd9f45c146c0543665f04409a8ba182e
dvij542 <dvij.kalaria@gmail.com>
2021-06-04 22:18:53 +0530

    changes

--- active_learning.py
+++ active_learning.py
@@ -97,6 +97,14 @@ def fine_tune(opt, model, device, hyp):
     train_path = data_dict['train']
     test_path = data_dict['val']
 
+    # Freeze
+    freeze = []  # parameter names to freeze (full or partial)
+    for k, v in model.named_parameters():
+        v.requires_grad = True  # train all layers
+        if any(x in k for x in freeze):
+            print('freezing %s' % k)
+            v.requires_grad = False
+
     # Optimizer
     nbs = 64  # nominal batch size
     accumulate = max(round(nbs / total_batch_size), 1)  # accumulate loss before optimizing
