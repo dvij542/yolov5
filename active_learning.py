04994078063e8e9802cd15b49223bd56add93389
dvij542 <dvij.kalaria@gmail.com>
2021-06-07 18:47:11 +0530

    changes

--- active_learning.py
+++ active_learning.py
@@ -407,8 +407,8 @@ def get_map_of_labels(opt, model_path, path_filter, device, hyp):
     gs = max(int(model.stride.max()), 32)  # grid size (max stride)
     nl = model.model[-1].nl  # number of detection layers (used for scaling hyp['obj'])
     imgsz, imgsz_test = [check_img_size(x, gs) for x in opt.img_size]  # verify imgsz are gs-multiples
-
-    dataloader = create_dataloader(path_filter, imgsz, 1, gs, opt, pad=0.5, rect=True, prefix='test')[0]
+    task = 'test'
+    dataloader = create_dataloader(path_filter, imgsz, 1, gs, opt, pad=0.5, rect=True, prefix=colorstr(f'{task}: '))[0]
     loss = 0
     for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
         img = img.to(device, non_blocking=True)
